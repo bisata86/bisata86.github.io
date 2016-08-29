032e0f95dfdd9f6b3cbbef3095030b720c312947
davide biasutto <bisata@gmail.com>
2016-08-29 22:57:12 +0200

    molto meglio

--- bounce2/bounce.js
+++ bounce2/bounce.js
@@ -18,7 +18,7 @@ $( document ).ready(function() {
   enemyImage.onload = function () {
     enemyReady = true;
   };
-  enemyImage.src = "./enemy.png";
+  enemyImage.src = "./up.png";
   canvas = document.createElement("canvas");
   ctx = canvas.getContext("2d");
   canvasWidth = $(window).width();
@@ -27,10 +27,10 @@ $( document ).ready(function() {
   canvas.height = canvasHeight;
   enemy.radius = 40;
   enemy.x= canvasWidth/2
-  enemy.y= canvasHeight/2
+  enemy.y= canvasHeight*3/4;
+  enemy.direction = 0;
     $('body').append(canvas);
-    $('body').append('<div class="bar"></div>');
-    $('.bar').append('<div class="percentage"></div>');
+    $('body').append("<div class='go'></div>");
 	$('body').on('click', 'canvas', function(event) {
 		//event.preventDefault();
 
@@ -39,25 +39,11 @@ $( document ).ready(function() {
 		// setTimeout(function(){ explode(temp) }, 1000);
 		//drawAll()
 	});
-	$('body').on('touchstart', 'canvas', function(event) {
-    if(distanceBetween2(enemy,{x:event.originalEvent.touches[0].pageX,y:event.originalEvent.touches[0].pageY})>enemy.radius+10) {
-		shapes.push({x:event.originalEvent.touches[0].pageX,y:event.originalEvent.touches[0].pageY,radius:salamoize,color:getRandomColor(),direction:'none'})
-		loadInterval =  setInterval(function(){
-      if(distanceBetween2(enemy,{x:event.originalEvent.touches[0].pageX,y:event.originalEvent.touches[0].pageY})>(shapes[shapes.length-1].radius)+enemy.radius)
-      if(storedSalamoize>=0) {
-			shapes[shapes.length-1].radius = salamoize;
-			salamoize=salamoize+1
-			storedSalamoize=storedSalamoize-3;
-      }
-		}, vel);
-  }
+	$('body').on('touchstart', '.go', function(event) {
+      enemy.moving = true;
 	});
-	$('body').on('touchend', 'canvas', function(event) {
-		clearInterval(loadInterval)
-		salamoize = initialSalamoize;
-		//shapes.push({x:event.pageX,y:event.pageY,radius:100,color:'red',direction:'none'})
-		explode(shapes.length)
-		//setTimeout(function(){ explode(temp) }, 1000);
+	$('body').on('touchend', '.go', function(event) {
+		enemy.moving = false;
 	});
 
 	drawAll();
@@ -82,104 +68,10 @@ var drawCirlce= function(centerX,centerY,radius) {
       ctx.fill();
 }
 var drawAll= function() {
-	$('.percentage').css({
-	width: storedSalamoize+'%'});
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	var ancora = true;
-  var winLevel = true;
-	$.each(shapes, function(index, val) {
-	    ctx.beginPath();
-      ctx.arc(val.x, val.y, val.radius, 0, 2 * Math.PI, false);
-      ctx.fillStyle = val.color;
-      ctx.fill();
-      if(val.direction!='none' && val.radius!=0) {
-      	if(val.direction=='n') {
-      		val.y = Math.abs(val.y-val.radius/2)
-      	}
-       	if(val.direction=='s') {
-      		val.y = Math.abs(val.y+val.radius/2)
-      	}
-       	if(val.direction=='e') {
-      		val.x = Math.abs(val.x+val.radius/2)
-      	}
-       	if(val.direction=='w') {
-      		val.x = Math.abs(val.x-val.radius/2)
-      	}
-      	if(val.y<val.radius) {
-      		val.direction='s'
-      		val.radius = val.radius*0.9;
-      	}
-      	if(val.y>canvasHeight-val.radius) {
-      		val.direction='n'
-      		val.radius = val.radius*0.9;
-      	}
-      	if(val.x>canvasWidth-val.radius) {
-      		val.direction='w'
-      		val.radius = val.radius*0.9;
-      	}
-      	if(val.x<0+val.radius) {
-      		val.direction='e'
-      		val.radius = val.radius*0.9;
-      	}
-      }
-      if(val.radius<5) {
-      	val.radius=0;
-      }
-      if(distanceBetween2(enemy,val)<enemy.radius/2) {
-        //if (confirm('colpito')) {
-        alert('next level')
-         ancora = false;
-         setTimeout(function(){
-            shapes = [];
-            enemy.x= canvasWidth/2
-             enemy.y= canvasHeight/2
-            enemyVel = enemyVel*2;
-            storedSalamoize = 100;
-            drawAll();
-
-          },1000);
-
-        //}
-      }
-       if(distanceBetween2(enemy,val)<enemy.radius+10) {
-
-
-
-        if(enemy.x > val.x) {
-          enemy.x = enemy.x+enemyVel//+val.radius+;
-        }
-        if(enemy.y > val.y) {
-          enemy.y = enemy.y+enemyVel//val.radius;
-        }
-        if(enemy.x < val.x) {
-          enemy.x = enemy.x-enemyVel//-val.radius;
-        }
-        if(enemy.y < val.y) {
-          enemy.y = enemy.y-enemyVel//-val.radius;
-        }
-
-      }
-      if(enemy.x<0 || enemy.y>canvasHeight || enemy.y<0 || enemy.x>canvasWidth) {
-          setTimeout(function(){
-             enemy.x= canvasWidth/2
-             enemy.y= canvasHeight/2
-           }, 1000);
-      }
-      if(val.radius<6) {
-        val.radius = 0;
-      }
-      if (val.radius>0) {
-        winLevel = false;
-      }
-
-	});
-  if(winLevel==true && storedSalamoize <=0) {
-    if(!reloadGame) {
-    setTimeout(function(){ window.location.reload(); }, 1000);
-    reloadGame = true;
-    }
-    setTimeout(function(){ ancora = false; }, vel);
-    enemyImage.src = "./enemy2.png";
+  if(enemy.moving) {
+    enemy.y=enemy.y-1;
   }
 	if(ancora) {
 		setTimeout(function(){ drawAll() }, vel);
