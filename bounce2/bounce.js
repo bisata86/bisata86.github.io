67d561dbd36cb5b36c968cfe35102b80d28d8faa
davide biasutto <bisata@gmail.com>
2016-08-31 18:26:09 +0200

    molto meglio

--- bounce2/bounce.js
+++ bounce2/bounce.js
@@ -47,8 +47,13 @@ $( document ).ready(function() {
   canvas = document.createElement("canvas");
 
   ctx = canvas.getContext("2d");
-  canvasWidth = $(window).width();
-  canvasHeight = $(window).height();
+  if(isMobile) {
+    canvasWidth = $(window).width();
+    canvasHeight = $(window).height();
+  } else {
+    canvasWidth = $(window).width();;
+    canvasHeight = $(window).height();
+  }
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
   var lines = (canvasHeight)/5;
@@ -82,11 +87,13 @@ $( document ).ready(function() {
   $('body').on('touchend', '.left', function(event) {
      clearInterval(leftInterval)
   });
-  $('body').on('click touchstart', '.popup', function(event) {
+  $('body').on('click touchstart', '.content', function(event) {
      $('.popup').fadeOut('200');
         obstacles = [];
         pedonali = [];
         shapes = [];
+        bonus = [];
+
         for (var i = 0; i < canvasHeight; i=i+lines) {
           shapes.push({x:(canvasWidth/2)-4,y:i})
         };
@@ -119,7 +126,7 @@ $( document ).ready(function() {
 
         if(hits==2)
         enemy.direction = (event.originalEvent.touches[0].pageX-startEventVolante.x)/15
-        if(hits==1)
+        else if(hits==1)
         enemy.direction = (event.originalEvent.touches[0].pageX-startEventVolante.x)/5
         else  enemy.direction = event.originalEvent.touches[0].pageX-startEventVolante.x
         $('.vol').css({
@@ -132,7 +139,10 @@ $( document ).ready(function() {
 });
 
 var addControls = function() {
-    $('body').append("<div class='go'></div>");
+
+  if($('.metri').length==0) {
+
+  $('body').append("<div class='go'></div>");
   $('body').append("<div class='metri'></div>");
 
   if(isMobile) {
@@ -142,6 +152,7 @@ var addControls = function() {
     $('body').append("<div class='left'></div>");
     $('body').append("<div class='right'></div>");
   }
+}
 }
 
 var placeEnemy = function() {
@@ -176,7 +187,7 @@ var drawAll= function() {
   counter++;
   $('.metri').html(parseInt(metri)+'<div> mt</div>')
   if(counter%100==0) modifier++;
-  if(parseInt(metri)%50==0) {
+  if(parseInt(metri)%30==0) {
     if(fatto) {
     obstacles.push({x:getRandomInt(20,canvasWidth-20),y:0})
     fatto = false;
@@ -260,14 +271,14 @@ var drawAll= function() {
   if (37 in keysDown) { // left
     if(hits==2)
     enemy.direction = enemy.direction-1
-    if(hits==1)
+    else if(hits==1)
     enemy.direction = enemy.direction-3
     else  enemy.direction = enemy.direction-5
   }
   if (39 in keysDown) { //  right
-     if(hits==2)
+    if(hits==2)
     enemy.direction = enemy.direction+1
-    if(hits==1)
+    else if(hits==1)
     enemy.direction = enemy.direction+3
     else  enemy.direction = enemy.direction+5
   }
