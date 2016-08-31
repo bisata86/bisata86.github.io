a4519f196a5bb8e123f5dc24a0b3c51b3655e83a
davide biasutto <bisata@gmail.com>
2016-08-31 15:30:26 +0200

    molto meglio

--- bounce2/bounce.js
+++ bounce2/bounce.js
@@ -13,6 +13,7 @@ var reloadGame = false;
 var salamoize = initialSalamoize;
 var enemyReady = false;
 var holeReady = false;
+var wrenchReady = false;
 var enemyImage;
 var holeImage;
 var vel =23;
@@ -31,6 +32,13 @@ $( document ).ready(function() {
     enemyReady = true;
   };
   enemyImage.src = "./car.png";
+
+  wrenchImage = new Image();
+  wrenchImage.onload = function () {
+    wrenchReady = true;
+  };
+  wrenchImage.src = "./wrench.png";
+
   holeImage = new Image();
   holeImage.onload = function () {
     holeReady = true;
@@ -90,6 +98,8 @@ $( document ).ready(function() {
        counter = 0;
        modifier = 1;
        metri = 1;
+       hits = 0;
+       enemyImage.src = "./car.png";
         $('.vol').css({
               '-moz-transform':'rotate('+enemy.direction+'deg)',
               '-webkit-transform':'rotate('+enemy.direction+'deg)'
@@ -152,7 +162,10 @@ var counter = 0;
 var modifier = 1;
 var metri = 1;
 var pedonali = [];
-  var fatto = true;
+var bonus = [];
+var hits = 0;
+var fatto = true;
+var fatto2 = true;
 var drawAll= function() {
   counter++;
   $('.metri').html(parseInt(metri)+'<div> mt</div>')
@@ -166,6 +179,12 @@ var drawAll= function() {
   if(parseInt(metri)%250==0) {
     pedonali.push({x:50,y:-100})
   }
+  if(parseInt(metri)%160==0) {
+    if(fatto2) {
+    bonus.push({x:getRandomInt(10,canvasWidth-10),y:0})
+    fatto2 = false;
+    }
+  } else fatto2 = true;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawRect(0,0,15,canvasHeight)
   drawRect(canvasWidth-15,0,15,canvasHeight)
@@ -194,12 +213,30 @@ var drawAll= function() {
       };
       val.y=val.y+modifier;
   });
+  $.each(bonus, function(index, val) {
+      ctx.drawImage(wrenchImage, val.x-15, val.y-15, 50, 50);
+      val.y=val.y+modifier;
+      if(distanceBetween2(enemy,val)<20) {
+          val.x = -100;
+          hits = 0;
+          enemyImage.src = "./car.png";
+
+
+      }
+  });
   $.each(obstacles, function(index, val) {
       ctx.drawImage(holeImage, val.x-15, val.y-15, 30, 30);
       val.y=val.y+modifier;
       if(distanceBetween2(enemy,val)<20) {
+        val.x = -100;
+        hits++;
+        if(hits==2) {
         ancora=false;
         alert('<div class="start">'+parseInt(metri)+' meters</div>')
+        } else {
+          enemyImage.src = "./car2.png";
+        }
+
       }
   });
 
